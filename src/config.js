48e24e3f68522560c3d145c3c68cec4f0e9d1b65
Adam A <adam@hack.bg>
2024-07-31 20:06:23 +0300

    chore(config): default to 0.41 testnet

--- src/config.js
+++ src/config.js
@@ -6,14 +6,15 @@ export const DEFAULT_PAGE_LIMIT = 25
 export const DEFAULT_PAGE_OFFSET = 0
 
 export const CHAIN_ID =
-  process.env.CHAIN_ID
+  process.env.CHAIN_ID || 'housefire-envelope.b8f955720ab'
 
 export const PRE_UNDEXER_RPC_URL =
-  process.env.PRE_UNDEXER_RPC_URL || "https://rpc.namada.tududes.com/" || "http://51.159.167.32:26657/";
+  process.env.PRE_UNDEXER_RPC_URL || 'https://rpc.knowable.run/'
+  //process.env.PRE_UNDEXER_RPC_URL || "https://rpc.namada.tududes.com/" || "http://51.159.167.32:26657/";
   //process.env.PRE_UNDEXER_RPC_URL || "http://51.159.167.32:26657/";
 
 export const POST_UNDEXER_RPC_URL =
-  process.env.POST_UNDEXER_RPC_URL || "https://rpc.namada.tududes.com/";
+  process.env.POST_UNDEXER_RPC_URL || PRE_INDEXER_RPC_URL;
 
 export const DATABASE_URL =
   process.env.DATABASE_URL || 'postgres://postgres:insecure@localhost:5432'
